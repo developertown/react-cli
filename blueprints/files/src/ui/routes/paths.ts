ca4e579e54feba167411a33fc2d0b2dab1819320
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-02-13 09:15:31 -0500

    more cleanup

--- blueprints/files/src/ui/routes/paths.ts
+++ blueprints/files/src/ui/routes/paths.ts
@@ -1,13 +1,12 @@
-// Top level routes
-export const rootPath = '/';
-export const loginPath = '/login';
-export const adminPath = '/admin';
-export const invitationsPath = '/invitations';
-export const organizationsPath = '/organizations';
-export const usersPath = '/users';
-export const projectsPath = '/projects';
-export const directoryPath = '/directory';
-export const openSourcePath = '/open-source';
-export const tasksPath = '/tasks';
-export const requestOrgAccessSuccessPath = '/request-access-for-organization/success';
-export const requestOrgAccessPath = '/request-access-for-organization';
+export const paths = {
+  root: { path: '/' }  ,
+  todos: {
+    path: '/todos',
+    forStatus: { 
+      path(status){ 
+        return `/todos/${status}`; 
+      }, 
+      name: '/todos/:status' 
+    },
+  },
+}
--- blueprints/files/src/ui/routes/projects/list/header/routes.ts
+++
@@ -1,6 +0,0 @@
-export enum PROJECT_ROUTES {
-  OWN = '/projects/own',
-  ORGANIZATION = '/projects/organization',
-  ARCHIVED = '/projects/archived',
-  ALL = '/projects/all',
-}
