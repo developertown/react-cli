8d37c238dad145d34f2aa97406a9fa99a8496e45
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-04-15 14:42:22 -0400

    add command

---
+++ packages/react-cli/src/commands/fetch-configs.ts
@@ -0,0 +1,29 @@
+import { Command } from '@oclif/command';
+import { ensureDependencies } from '../tasks/ensure-dependiencies';
+import Listr from 'listr';
+import { downloadTSConfigFiles } from '../tasks/download-ts-config';
+import { exec } from '../utils/shell';
+
+export class FetchConfigsCommand extends Command {
+  static description = 'Fetches config files for linting and typescript projects';
+
+  static examples = ['$ react fetch-configs'];
+
+  async run() {
+    await ensureDependencies();
+
+    let path = './';
+    let tasks = new Listr([
+      {
+        title: 'Downloading shared config for DeveloperTown',
+        task: () => downloadTSConfigFiles(path),
+      },
+      {
+        title: 'Installing Dependencies',
+        task: () => exec(`yarn install`),
+      },
+    ]);
+
+    await tasks.run();
+  }
+}
