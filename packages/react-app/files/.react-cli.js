70f3121ec2a5340718ba08b637a1e00402be822a
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-05-17 11:25:43 -0400

    add .react-cli.js to buleprint

---
+++ packages/react-app/files/.react-cli.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+module.exports = {
+  blueprintVersion: '',
+  features: {
+    <% if (testing) { %>
+      testing: true,
+    <% } %>
+    <% if (jsonapi) { %>
+      jsonapi: true,
+    <% } %>
+    <% if (auth0) { %>
+      auth0: true,
+    <% } %>
+    <% if (i18n) { %>
+      i18n: true,
+    <% } %>
+    <% if (redux) { %>
+      redux: true,
+    <% } %>
+  },
+  styles: {
+    <% if (materialUi) { %>
+      materialUi: true,
+    <% } %>
+  },
+}
